c3e920de8fb5281cceab684d040852382072ad3e
HeongiLee <gjsrl1@gmail.com>
2020-03-09 16:55:57 +0900

    2020-03-09 :: basic grammar

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'cupertino_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,19 +10,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        primaryColor: Colors.yellow,
+        accentColor: Colors.green,
+        canvasColor: Colors.purple,
       ),
-      home:HelloPage('Hello World')
-    );
+      home: HelloPage('Hello World'));
+      // home: CupertinoPage());
   }
 }
 
@@ -37,25 +32,47 @@ class HelloPage extends StatefulWidget {
 
 class _HelloPageState extends State<HelloPage> {
   String _message = 'Hello World...';  //변수명 앞에 언더스코어를 붙이면 private, 없으면 기본적으로 public.
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //floatingActionButton 으로 우측 하단에 버튼을 생성할 수 있습니다.
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),   //child의 Icon클래스를 하나 만들어 아이콘 모양을 정할 수 있습니다.
-        onPressed: _changeMessage),
-      appBar: AppBar(
-        title: Text(widget.title),
-      ), //appBar
-      body: Text(_message, style: TextStyle(fontSize: 30)),
+        child: Icon(Icons.add),
+        onPressed: _changeMessage
+        ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_message, style: TextStyle(fontSize: 30),),
+              Text('$_count', style: TextStyle(fontSize: 30)),
+              RaisedButton(
+                child: Text('화면 이동'),
+                onPressed: (){
+                  //push는 화면이 위로 하나씩 올라오는 것.
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context) => CupertinoPage())
+                    ); 
+                  //pop은 위에 있던 화면이 하나씩 빠지는 것.
+                  // Navigator.pop(context);
+                }),
+            ],
+          ),
+        ),
     );
-    }
+  }
 
     //메서드 하나를 생성해 floatingActionButton을 누르면 헬로 월드로 setState되게 바꿔보기 예제.
-    void _changeMessage(){
-      setState(() {
+    void _changeMessage(){ 
+      setState(() {        
         _message = '헬로 World!!';
+        _count++;
       });
   }
 }
